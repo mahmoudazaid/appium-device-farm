fed7253c023eeb14fb1296ad03bcdd5fffdc2757
Mahmoud Abuzaid <mahmoud.abuzaid@uniscon.com>
2024-11-22 17:04:29 +0100

    fix path

--- start.sh
+++ start.sh
@@ -12,24 +12,39 @@ NC='\033[0m'       # No Color
 #======================#
 # Default configuration values
 APPIUM_PORT="${APPIUM_PORT:-4723}"           # Default Appium port
-KEEP_ALIVE="${KEEP_ALIVE:-600}"              # Default keep-alive in ms
-HUB_ADDRESS="${HUB_ADDRESS:-}"                # Optional hub IP
-HUB_PORT="${HUB_PORT:-4723}"                 # Default Hub port
-BASE_PATH="${BASE_PATH:-}"                   # Default base path
+KEEP_ALIVE="${KEEP_ALIVE:-600}"             # Default keep-alive in ms
+HUB_ADDRESS="${HUB_ADDRESS:-}"              # Optional hub IP
+HUB_PORT="${HUB_PORT:-4723}"                # Default Hub port
+BASE_PATH="${BASE_PATH:-}"                  # Default base path
+PLATFORM="${PLATFORM:-both}"                # Default platform
 
 #==============#
 # Start Appium #
 #==============#
-printf "${G}==> ${BL}Starting Appium on port ${YE}${APPIUM_PORT}${G} with keep-alive ${YE}${KEEP_ALIVE}${G} ms <==${NC}\n"
+printf "${G}==> ${BL}Starting Appium on port ${YE}${APPIUM_PORT}${G} with keep-alive ${YE}${KEEP_ALIVE}${G} ms and platform ${YE}${PLATFORM}${G} <==${NC}\n"
 
 #===============================#
 # Build the base Appium command #
 #===============================#
-appium_command="appium server \
-    --keep-alive ${KEEP_ALIVE} \
-    --use-plugins=device-farm \
-    --plugin-device-farm-platform=android \
-    --port ${APPIUM_PORT}"
+appium_command="appium server --use-plugins=device-farm"
+
+#===================================#
+# Add Platform Configuration        #
+#===================================#
+printf "${G}==> ${BL}Configuring platform to ${YE}${PLATFORM}${NC}\n"
+appium_command+=" --plugin-device-farm-platform=${PLATFORM}"
+
+#===================================#
+# Add Keep Alive Configuration      #
+#===================================#
+printf "${G}==> ${BL}Configuring --keep-alive to ${YE}${KEEP_ALIVE}${NC}\n"
+appium_command+=" --keep-alive ${KEEP_ALIVE}"
+
+#=================#
+# Add Appium Port #
+#=================#
+printf "${G}==> ${BL}Configuring --port to ${YE}${APPIUM_PORT}${NC}\n"
+appium_command+=" --port ${APPIUM_PORT}"
 
 #=======================================================#
 # Add hub configuration only if HUB_ADDRESS is provided #
@@ -43,6 +58,8 @@ fi
 # Add base path if provided #
 #===========================#
 if [[ -n "${BASE_PATH}" ]]; then
+    # Ensure BASE_PATH doesn't end with "/"
+    BASE_PATH="${BASE_PATH%/}"
     printf "${G}==> ${BL}Using base path: ${YE}${BASE_PATH}${NC}\n"
     appium_command+=" --base-path ${BASE_PATH}"
 fi
